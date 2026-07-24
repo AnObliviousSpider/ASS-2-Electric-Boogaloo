a9783ab23fde7a4a4e80381f6070ce5e5c108161
CuriousBlobfish <blobfishcurious@gmail.com>
2026-07-24 16:31:56 +0100

    added all bins hooked up to mood dialogue

--- Scripts/PeggleBoard/Bin/peggleballbin.gd
+++ Scripts/PeggleBoard/Bin/peggleballbin.gd
@@ -4,15 +4,16 @@ extends Area2D
 
 signal ball_caught(ball: Node2D, bin_emotion: int)
 
-@export_enum("Happy", "Dejected", "Flirty", "Angry") var what_emotion_to_resoond_to: int = 0 :
+@export_enum("Happy", "Dejected", "Flirty", "Angry") var what_emotion_to_respond_with: int = 0 :
 	set(value):
-		what_emotion_to_resoond_to = value
-		$Sprite2D.texture = load("res://Assets/Art/Game/BinSprites/" + GameData.emotions.keys()[what_emotion_to_resoond_to] + "Bin.png")
+		what_emotion_to_respond_with = value
+		if $Sprite2D and GameData.emotions.keys()[what_emotion_to_respond_with]:
+			$Sprite2D.texture = load("res://Assets/Art/Game/BinSprites/" + GameData.emotions.keys()[what_emotion_to_respond_with] + "Bin.png")
 
 
 
 func _ready() -> void:
-	$Sprite2D.texture = load("res://Assets/Art/Game/BinSprites/" + GameData.emotions.keys()[what_emotion_to_resoond_to] + "Bin.png")
+	$Sprite2D.texture = load("res://Assets/Art/Game/BinSprites/" + GameData.emotions.keys()[what_emotion_to_respond_with] + "Bin.png")
 	body_entered.connect(_on_body_entered)
 	if get_child_count() == 3:
 		GameData.connect("emotion_changed", get_child(2).emotion_effect())
@@ -24,6 +25,8 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.get_meta("ball_resolved", false) == true:
 		return
 	
-	ball_caught.emit(body, what_emotion_to_resoond_to)
-
+	ball_caught.emit(body, what_emotion_to_respond_with)
+	
+	EventBus.dialogue_mood_triggered.emit(GameData.emotions.keys()[what_emotion_to_respond_with], LevelManager.level)
+	print("triggered mood dialogue: ", GameData.emotions.keys()[what_emotion_to_respond_with])
 	
--- Scripts/UI/Dialogue/mood_dialogue_box.gd
+++ Scripts/UI/Dialogue/mood_dialogue_box.gd
@@ -8,24 +8,24 @@ extends Control
 
 
 var dialogue_moods: Dictionary = {
-	"HAPPY": [
+	"Happy": [
 		"Im happy",
 		"*happy noise*",
 		"Yippyyyyy",
 		"MATH IS FUN",
 	],
-	"FLIRTY": [
+	"Flirty": [
 		"I lik u lowkey",
 		"Are you Wi‑Fi? Because I’m feeling strong signals and zero common sense. (AI)",
 		"How do i rizz someone",
 		"touch grass? no. touch me (WHY IS MY AI FREAKY GAHDAMN)",
 	],
-	"ANGRY": [
+	"Angry": [
 		"BARK BARK BARK BARK GRRRRR",
 		"D:<",
 		"GRRRR BARK",
 	],
-	"DEJECTED": [
+	"Dejected": [
 		"MC, I know we don't get along.. but I think I'm cooked.",
 		"Um.. my rizz is on airplane mode. (AI GENERATED)",
 	],
@@ -40,12 +40,15 @@ func _ready() -> void:
 		dialogue_box.on_display_audio = on_display_audio
 	if on_text_audio:
 		dialogue_box.on_text_audio = on_text_audio
-			
+
 func _on_dialogue_mood_triggered(mood: String, level: int) -> void:
 	DialogueManager.open_dialogue()
 	# Dialogue system for mood mode
-	var dialogue : String = dialogue_moods[mood].pick_random()
-	dialogue_box.display_dialogue(dialogue)
-	await EventBus.dialogue_next
-	dialogue_box.hide_dialogue()
-	DialogueManager.close_dialogue()
+	if mood in dialogue_moods.keys():
+		var dialogue : String = dialogue_moods[mood].pick_random()
+		dialogue_box.display_dialogue(dialogue)
+		await EventBus.dialogue_next
+		dialogue_box.hide_dialogue()
+		DialogueManager.close_dialogue()
+	else:
+		printerr("Mood: ", mood, " not in dialogue_moods dictionary")
